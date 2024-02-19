3d74ba12d8848fb3b3f7907b0dab15f19ce93539
Seth Millan <millanseth@gmail.com>
2024-02-18 22:51:26 -0600

    update: Se programo la clase Position

--- Position.cs
+++ Position.cs
@@ -3,5 +3,12 @@ namespace Tetris
 {
     public class Position
     {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
     }
 }
