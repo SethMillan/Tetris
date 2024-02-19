a4f189fb9d6ac71cfa0b3857acd53a8fffe0858f
Seth Millan <millanseth@gmail.com>
2024-02-19 15:24:15 -0600

    add: Se agrego la carpeta Assets

--- obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1687f084a85d2864a19959a00dc1f97ad03fa080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82129dcbfb047238d5f863589d49357588ac4127")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
