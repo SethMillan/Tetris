53e3a4247acb4ac4175532f7baec665b4af543b1
Seth Millan <millanseth@gmail.com>
2024-02-19 13:43:53 -0600

    update: Se escribio la clase TBlock

--- TBlock.cs
+++ TBlock.cs
@@ -1,6 +1,19 @@
 ﻿namespace Tetris
 {
-    public class TBlock
+    public class TBlock:Block
     {
+
+        private readonly Position[][] tiles = new Position[][]
+        {
+           new Position[] { new(0, 1), new(1, 0), new(1, 1), new(1, 2) },
+            new Position[] { new(0, 1), new(1, 1), new(1, 2), new(2, 1) },
+            new Position[] { new(1, 0), new(1, 1), new(1, 2), new(2, 1) },
+            new Position[] { new(0, 1), new(1, 0), new(1, 1), new(2, 1) }
+
+        };
+        public override int id => 6;
+        protected override Position starOffSet => new Position(0, 3);
+        protected override Position[][] Tiles => tiles;
+
     }
 }
