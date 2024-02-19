e4bbac9773df714ba7f040dba9045a0e7ef70929
Seth Millan <millanseth@gmail.com>
2024-02-18 23:39:56 -0600

    add: Se agrego la clase JBlock

--- obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f783f0c195f0a151d01e19c5512535b2dd206ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478c378d83fcc583536e91292fb0543e54e51871")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
